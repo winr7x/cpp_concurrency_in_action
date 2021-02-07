b2d20ccea4b4ae37723c3bb263bf71c3be3b4f51
winr7x <winr7x@gmail.com>
2021-02-07 22:30:14 +0300

    Update 02.01_2.1.1_most_unpleasant_syntax_case.cpp

--- listings/02.01_2.1.1_most_unpleasant_syntax_case.cpp
+++ listings/02.01_2.1.1_most_unpleasant_syntax_case.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
+#include <thread>
 
 class C
 {
 public:
     void operator()() const
     {
+        std::cout << ++x << std::endl;
     }
+private:
+  static int x;
 };
+
+int C::x = 1;
 
 int main()
 {
-  double f(C());     // The most unpleasant syntax case:
-                     // You wanted to pass a temporary object,
-                     // but it parsed as function declaration:
-                     // double f ( TODO type );
+  std::thread f1(C());    // The most unpleasant syntax case:
+                          // You wanted to pass a temporary object,
+                          // but it parsed as function declaration:
+                          // double f ( C (*)() );
+                          // Thread is not started here
                  
-  double f((C()));   // compilation failure. 
-                     // error: cannot convert 'C' to 'double' in initialization
+  std::thread f2((C()));  // ok (Way 1)
+                          // Thread is started.
+                          // Thread represented by operator() method of temporary object
   
   
-  double f({C()});   // compilation failure.
-                     // error: cannot convert '<brace-enclosed initializer list>' to 'double' in initialization
+  std::thread f3{C()};    // ok (Way 2)
+                          // Thread is started
+                          // Thread represented by operator() method of temporary object
+
+  C c1;                          
+  std::thread f4(c1);     // ok
+                          // Thread is started
+                          // Thread represented by operator() method of object c1
+                          
+  
+  f2.join();
+  f3.join();
+  f4.join();  
 }
+
+// Output example 1:
+// 243
+    
+// Output example 2:
+// 2
+// 3
+// 4
+
+// Output example 4:
+// 23
+// 4
