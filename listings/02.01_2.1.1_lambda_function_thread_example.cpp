2cd4f4837225deef7685752abb2a5ba85100bad0
winr7x <winr7x@gmail.com>
2021-02-08 22:21:12 +0300

    Create 02.01_2.1.1_lambda_function_thread_example.cpp

---
+++ listings/02.01_2.1.1_lambda_function_thread_example.cpp
@@ -0,0 +1,25 @@
+// g++ -Os -s -Wall -Wextra -std=c++17 file.cpp -pthread -o program
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main()
+{
+  // Lambda thread function example
+  std::thread t([]{
+      std::cout << "in thread" << std::endl;
+  });
+  // You must decide attach or detach thread at the time of creation
+  t.detach();
+  
+  // Sleep 50ms
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  
+  // Sleep 50ms
+  using namespace std::chrono_literals;
+  std::this_thread::sleep_for(50ms);
+}
+
+// Output:
+// in thread
