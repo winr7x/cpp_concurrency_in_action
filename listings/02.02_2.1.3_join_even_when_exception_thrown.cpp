ebf48623a6020bd24a099f0965fee809ecb9aab5
winr7x <winr7x@gmail.com>
2021-02-13 21:35:42 +0300

    Update 02.02_2.1.3_join_even_when_exception_thrown.cpp

--- listings/02.02_2.1.3_join_even_when_exception_thrown.cpp
+++ listings/02.02_2.1.3_join_even_when_exception_thrown.cpp
@@ -1,3 +1,40 @@
+// g++ -Os -Wall -Werror -s -std=c++17 src.cpp -pthread -o program
+
+#include <iostream>
+#include <thread>
+
+// The main idea: to show simple example with std::thread
+
+void hello()
+{
+    std::cout << "Hello Concurrent World\n";
+    
+    // Why \n is used instead of std::endl ?
+    // Because std::flush (which is included in std::endl) is not necessarry here.
+    
+    // When std::endl (with std::flush) is necessary ?
+    //
+    // Example 1:
+    //   To display title before some operation like:
+    //     std::cout << "Beginning computations..." << std::endl;
+                      // without buffer flushing you may encounter a situation when
+                      // both lines are displayed at the same time when computations are finished
+    //     computations();
+    //     std::cout << " done!\n";
+    //   https://stackoverflow.com/questions/22026751/c-force-stdcout-flush-print-to-screen
+    // 
+    // Example 2:
+    //   To display a progress bar
+    //   https://stackoverflow.com/questions/213907/stdendl-vs-n#comment6217683_214076
+    
+}
+
+int main()
+{
+    std::thread t(hello);  // !!HELLO THREAD EXAMPLE!!
+    t.join();              //
+}
+
 #include <iostream>
 #include <thread>
 
